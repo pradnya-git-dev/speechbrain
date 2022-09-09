a736e4b183c3100b03360282bca29957c7dc8e74
pradnya-git-dev <kandarkarp@gmail.com>
2022-09-09 17:45:20 -0400

    Update libritts_prepare.py

--- recipes/LibriTTS/exp5_t_spk_embs_2/libritts_prepare.py
+++ recipes/LibriTTS/exp5_t_spk_embs_2/libritts_prepare.py
@@ -62,7 +62,7 @@ def prepare_libritts(
     )
     extension = [".wav"]
 
-    speaker_counter = 2
+    speaker_counter = 1
 
     wav_list = list()
     for speaker_folder in os.listdir(train_folder):
