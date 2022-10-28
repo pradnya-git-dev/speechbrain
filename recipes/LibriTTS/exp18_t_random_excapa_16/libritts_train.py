1140a2cdcb43789f5b92225bed58806e8c15e4b4
pradnya-git-dev <kandarkarp@gmail.com>
2022-10-28 16:54:04 -0400

    Update libritts_train.py

--- recipes/LibriTTS/exp18_t_random_excapa_16/libritts_train.py
+++ recipes/LibriTTS/exp18_t_random_excapa_16/libritts_train.py
@@ -45,8 +45,7 @@ class Tacotron2Brain(sb.Brain):
         self.last_batch = None
         self.last_preds = None
         self.vocoder = HIFIGAN.from_hparams(source="speechbrain/tts-hifigan-ljspeech", savedir="tmpdir_vocoder", run_opts={"device": self.device})
-        self.spk_emb_encoder = EncoderClassifier.from_hparams(source="speechbrain/spkrec-xvect-voxceleb", 
-                                                              savedir="pretrained_models/spkrec-xvect-voxceleb", 
+        self.spk_emb_encoder = EncoderClassifier.from_hparams(source="speechbrain/spkrec-ecapa-voxceleb",
                                                               run_opts={"device": self.device})
         self.last_loss_stats = {}
         return super().on_fit_start()
