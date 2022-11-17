37def6703ecd1b3e5932f868168834c6b6f67e83
pradnya-git-dev <kandarkarp@gmail.com>
2022-11-17 12:10:21 -0500

    finalizing exp

--- recipes/LibriTTS/TTS/exp3_g2p/exp32_utterance_invariance/exp321_2_random_utterances/exp3211_tacotron2_ecapa/libritts_train.py
+++ recipes/LibriTTS/TTS/exp3_g2p/exp32_utterance_invariance/exp321_2_random_utterances/exp3211_tacotron2_ecapa/libritts_train.py
@@ -496,8 +496,8 @@ if __name__ == "__main__":
     sb.utils.distributed.run_on_main(
         compute_speaker_embeddings,
         kwargs={
-            "input_filepaths": [hparams["train_json"], hparams["valid_json"], hparams["test_json"]],
-            "output_file_paths": [hparams["train_speaker_embeddings_pickle"], hparams["valid_speaker_embeddings_pickle"], hparams["test_speaker_embeddings_pickle"]],
+            "input_filepaths": [hparams["train_json"], hparams["valid_json"]],
+            "output_file_paths": [hparams["train_speaker_embeddings_pickle"], hparams["valid_speaker_embeddings_pickle"]],
             "data_folder": hparams["data_folder"],
             "audio_sr": hparams["sample_rate"],
             "spk_emb_sr": hparams["spk_emb_sample_rate"]
