09600fdc1852918d28993ce6945d20a2918f33b3
pradnya-git-dev <kandarkarp@gmail.com>
2022-12-06 07:46:55 +0000

    finalizing exp

--- recipes/LibriTTS/TTS/exp7_loss_variations/exp74_spk_emb_loss/exp741_SCL/exp7411_tacotron2_ecapa/libritts_train.py
+++ recipes/LibriTTS/TTS/exp7_loss_variations/exp74_spk_emb_loss/exp741_SCL/exp7411_tacotron2_ecapa/libritts_train.py
@@ -53,7 +53,7 @@ class Tacotron2Brain(sb.Brain):
         )
 
         self.spk_emb_mel_spec_encoder = MelSpectrogramEncoder.from_hparams(
-          source="/content/drive/MyDrive/ecapa_tdnn/mel_spec_input",
+          source="/workspace/mstts_saved_models/ecapa_tdnn_mel_spec_80",
           run_opts={"device": self.device},
           freeze_params=True
         )
@@ -598,9 +598,10 @@ if __name__ == "__main__":
     sb.utils.distributed.run_on_main(
         compute_speaker_embeddings,
         kwargs={
-            "input_filepaths": [hparams["train_json"]],
+            "input_filepaths": [hparams["train_json"], hparams["valid_json"]],
             "output_file_paths": [
                 hparams["train_speaker_embeddings_pickle"],
+                hparams["valid_speaker_embeddings_pickle"],
             ],
             "data_folder": hparams["data_folder"],
             "audio_sr": hparams["sample_rate"],
@@ -656,7 +657,7 @@ if __name__ == "__main__":
     tacotron2_brain.fit(
         tacotron2_brain.hparams.epoch_counter,
         train_set=datasets["train"],
-        valid_set=datasets["train"],
+        valid_set=datasets["valid"],
         train_loader_kwargs=hparams["train_dataloader_opts"],
         valid_loader_kwargs=hparams["valid_dataloader_opts"],
     )
@@ -664,6 +665,6 @@ if __name__ == "__main__":
     # Test
     if "test" in datasets:
         tacotron2_brain.evaluate(
-            datasets["train"],
+            datasets["test"],
             test_loader_kwargs=hparams["test_dataloader_opts"],
         )
