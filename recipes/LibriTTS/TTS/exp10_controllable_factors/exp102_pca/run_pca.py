f316c184852eeffe1d646ecbca8a34e60b6f7e1c
pradnya-git-dev <kandarkarp@gmail.com>
2023-04-24 11:51:08 +0000

    saving progress

--- recipes/LibriTTS/TTS/exp10_controllable_factors/exp102_pca/run_pca.py
+++ recipes/LibriTTS/TTS/exp10_controllable_factors/exp102_pca/run_pca.py
@@ -77,82 +77,107 @@ tb_writer.add_embedding(
   tag="spk_embs_before_pca"
 )
 
-
+N_PRINCIPAL_COMPONETS = 2
 original_spk_embs = log_spk_embs.numpy()
-pca = PCA(n_components=2) # estimate only 2 PCs
-pca_spk_embs = pca.fit_transform(original_spk_embs)
 
-transformed_spk_embs = torch.from_numpy(pca_spk_embs)
+print("Max number of principal components to try: ", N_PRINCIPAL_COMPONETS)
+for N_COMPONENTS in range(1, N_PRINCIPAL_COMPONETS+1):
 
-tb_writer.add_embedding(
-  transformed_spk_embs,
-  metadata=log_spk_embs_labels,
-  tag="spk_embs_after_pca"
-)
+  print("Current number of principal components: ", N_COMPONENTS)
 
-fig, axes = plt.subplots(1,2)
-axes[0].scatter(original_spk_embs[:,0], original_spk_embs[:,1], c=log_spk_embs_labels)
-axes[0].set_xlabel('x1')
-axes[0].set_ylabel('x2')
-axes[0].set_title('Before PCA')
-axes[1].scatter(transformed_spk_embs[:,0], transformed_spk_embs[:,1], c=log_spk_embs_labels)
-axes[1].set_xlabel('PC1')
-axes[1].set_ylabel('PC2')
-axes[1].set_title('After PCA')
-plt.show()
+  pca = PCA(n_components=N_COMPONENTS) # estimate only 2 PCs
+  pca_spk_embs = pca.fit_transform(original_spk_embs)
 
-fig.savefig('pca.png')
+  transformed_spk_embs = torch.from_numpy(pca_spk_embs)
 
-# import pdb; pdb.set_trace()
-pca_vectors = pca.components_
-pca_eigenvalues = pca.singular_values_
+  tb_writer.add_embedding(
+    transformed_spk_embs,
+    metadata=log_spk_embs_labels,
+    tag=f"spk_embs_after_{N_COMPONENTS}_pca"
+  )
 
-# Effects for PC1
-pc_vector_1 = pca_vectors[0]
+  # fig, axes = plt.subplots(1,2)
+  # axes[0].scatter(original_spk_embs[:,0], original_spk_embs[:,1], c=log_spk_embs_labels)
+  # axes[0].set_xlabel('x1')
+  # axes[0].set_ylabel('x2')
+  # axes[0].set_title('Before PCA')
+  # axes[1].scatter(transformed_spk_embs[:,0], transformed_spk_embs[:,1], c=log_spk_embs_labels)
+  # axes[1].set_xlabel('PC1')
+  # axes[1].set_ylabel('PC2')
+  # axes[1].set_title('After PCA')
+  # plt.show()
 
-spk_ids = ["5895", "8297"]
-spk_emb_1 = random.sample(spk_embs_dict[spk_ids[0]], k=1)[0]
-spk_emb_2 = random.sample(spk_embs_dict[spk_ids[1]], k=1)[0]
+  # fig.savefig('pca.png')
 
-steps = 10
+  # import pdb; pdb.set_trace()
+  print("Extracting PCs and recording the effects of taking steps wrt eigenvalues.")
+  pca_vectors = pca.components_
+  pca_eigenvalues = pca.singular_values_
 
-gradual_spk_embs = list()
-gradual_spk_embs_labels = list()
+  spk_ids = ["5895", "8297"]
+  spk_emb_1 = random.sample(spk_embs_dict[spk_ids[0]], k=1)[0]
+  spk_emb_2 = random.sample(spk_embs_dict[spk_ids[1]], k=1)[0]
 
-gradual_spk_embs.append(spk_emb_1)
-gradual_spk_embs_labels.append(spk_ids[0])
 
-for i in range(steps):
-  pc_change = pca_vectors[1] + (i)
-  spk_emb_grad = spk_emb_1 + pc_change
-  gradual_spk_embs.append(spk_emb_grad)
+  for pci in range(len(pca_vectors)):
 
-  gradual_spk_embs_labels.append(spk_ids[0] + f"_step_{i}")
+    print(f"Working with PC {pci + 1}")
 
-gradual_spk_embs = torch.stack(gradual_spk_embs)
+    step_size = pca_eigenvalues[pci]
+    num_of_steps = 10
 
-PHRASE = "Mary had a little lamb"
-common_phrase_phoneme_list = g2p(PHRASE)
-common_phrase = " ".join(common_phrase_phoneme_list)
-common_phrase = "{" + common_phrase + "}"
-print(common_phrase)
+    while step_size > 1:
 
-phoneme_seqs = [common_phrase for i in range(gradual_spk_embs.shape[0])]
-mel_output_ms, mel_length_ms, alignment_ms = tacotron2_ms.encode_batch(phoneme_seqs, gradual_spk_embs)
-waveform_cp = hifi_gan.decode_batch(mel_output_ms)
+      gradual_spk_embs = list()
+      gradual_spk_embs_labels = list()
 
-for (a, waveform) in enumerate(waveform_cp):
+      gradual_spk_embs.append(spk_emb_1)
+      gradual_spk_embs_labels.append(spk_ids[0])
 
-  tb_writer.add_audio(
-      f"{gradual_spk_embs_labels[a]}.wav",
-      waveform.squeeze(1),
-      sample_rate=16000
-    )
+      print(f"Setting step size to {step_size}")
 
-tb_writer.add_embedding(
-  gradual_spk_embs,
-  metadata=gradual_spk_embs_labels,
-  tag="gradual_spk_embs"
-)
+      for i in range(num_of_steps):
+        pc_change = pca_vectors[pci] + (i * step_size)
+
+        spk_emb_grad_1 = spk_emb_1 + pc_change
+        gradual_spk_embs.append(spk_emb_grad_1)
+        gradual_spk_embs_labels.append(spk_ids[0] + f"_pc_{pci}_step_{step_size}_{i}")
+
+        spk_emb_grad_2 = spk_emb_2 + pc_change
+        gradual_spk_embs.append(spk_emb_grad_2)
+        gradual_spk_embs_labels.append(spk_ids[1] + f"_tpc_{N_COMPONENTS}_cpc_{pci}_step_{step_size}_{i}")
+
+      gradual_spk_embs = torch.stack(gradual_spk_embs)
+
+      PHRASE = "Mary had a little lamb"
+      common_phrase_phoneme_list = g2p(PHRASE)
+      common_phrase = " ".join(common_phrase_phoneme_list)
+      common_phrase = "{" + common_phrase + "}"
+      print(common_phrase)
+
+      phoneme_seqs = [common_phrase for i in range(gradual_spk_embs.shape[0])]
+      mel_output_ms, mel_length_ms, alignment_ms = tacotron2_ms.encode_batch(phoneme_seqs, gradual_spk_embs)
+      waveform_cp = hifi_gan.decode_batch(mel_output_ms)
+
+      for (a, waveform) in enumerate(waveform_cp):
+
+        tb_writer.add_audio(
+            f"{gradual_spk_embs_labels[a]}.wav",
+            waveform.squeeze(1),
+            sample_rate=16000
+          )
+
+      tb_writer.add_embedding(
+        gradual_spk_embs,
+        metadata=gradual_spk_embs_labels,
+        tag=f"grad_se_tpc_{N_COMPONENTS}_cpc_{pci}_step_size_{step_size}"
+      )
+
+      step_size = step_size / 2
+
+      print(f"Reduced step size to {step_size}")
+      print("Running exp with the new step size")
+
+
 
 print("DONE!")
