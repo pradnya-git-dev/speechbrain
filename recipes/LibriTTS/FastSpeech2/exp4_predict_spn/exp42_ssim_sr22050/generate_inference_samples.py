8b0aa6870db6988dea049eedce8c7b34cfaf267b
pradnya-git-dev <kandarkarp@gmail.com>
2023-05-10 17:40:00 +0000

    saving progress

--- recipes/LibriTTS/FastSpeech2/exp4_predict_spn/exp42_ssim_sr22050/generate_inference_samples.py
+++ recipes/LibriTTS/FastSpeech2/exp4_predict_spn/exp42_ssim_sr22050/generate_inference_samples.py
@@ -13,14 +13,11 @@ import string
 fastspeech2 = FastSpeech2.from_hparams(source="/content/drive/MyDrive/mstts_saved_models/FastSpeech2/exp4_predict_spn/fastspeech_starter_sr_20050_e500",
                                      hparams_file="/content/speechbrain/recipes/LibriTTS/FastSpeech2/exp4_predict_spn/exp42_ssim_sr22050/fs2_inference_hparams.yaml")
 
-hifi_gan = HIFIGAN.from_hparams(source="speechbrain/tts-hifigan-libritts-22050Hz", savedir="tmpdir_vocoder")
+hifi_gan = HIFIGAN.from_hparams(source="speechbrain/tts-hifigan-ljspeech", savedir="tmpdir_vocoder")
 
 # Take input text
 # import pdb; pdb.set_trace()
-input_text = "It was also suggested that it would take a substantial period of time for the secret service to build up the skills necessary to meet the problem"
-# input_text = "Child, what art thou? Cried the mother."
-# input_text = "Testing homographs. I like to read. I read that book twice."
-# input_text = "Testing homographs. I like to read. I read that book twice."
+input_text = "Were the leaders in this luckless change, though our own Baskerville, who was at work some years before them, went much on the same lines."
 
 print(input_text)
 # Convert text label into a phoneme label
@@ -31,5 +28,5 @@ print(input_text)
 # Pass phoneme label through the TTS pipeline
 mel_output, durations, pitch, energy = fastspeech2.encode_text(input_text)
 waveforms = hifi_gan.decode_batch(mel_output)
-torchaudio.save(os.path.join("/content", "ljspeech_1.wav"),waveforms.squeeze(1), 22050)
+torchaudio.save(os.path.join("/content", "fs2_site_1.wav"),waveforms.squeeze(1), 22050)
 
